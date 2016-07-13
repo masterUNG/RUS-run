6bffd82f5616fdee65e0ba7cef0dab69401cf35f
masterUNG <phrombutr@gmail.com>
2016-07-13 16:22:53 +0700

    Finish 13July

--- app/src/main/java/appewtc/masterung/rusrun/MainActivity.java
+++ app/src/main/java/appewtc/masterung/rusrun/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userEditText, passwordEditText;
     private ImageView imageView;
     private static final String urlLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
-
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
     }   // Main Method
 
     public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "Have Space",
+                    "Please Fill All Every Blank");
+
+        } else {
+            //No Space
+
+        }   //if
+
+
+
 
     }   // clickSign
 
